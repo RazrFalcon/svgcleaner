8706dc576e3b6f50d05672831a8f6c9a116bf527
Reizner Evgeniy <razrfalcon@gmail.com>
2017-06-13 15:23:47 +0300

    Version bump.

--- src/cli.rs
+++ src/cli.rs
@@ -202,7 +202,7 @@ pub fn prepare_app<'a, 'b>() -> App<'a, 'b> {
 
     App::new("svgcleaner")
         .help(include_str!("../data/help.txt"))
-        .version("0.9.0")
+        .version("0.10.0")
         .arg(Arg::with_name("in-file")
             .required(true)
             .index(1)
